45568b27463dfaa55206124e66d7dd623d804c79
Sunset3777 <aua52257@gmail.com>
2026-03-19 10:58:18 +0800

    refactor:style to typescript

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import TodoPage from './TodoPage.jsx'
+import TodoPage from './TodoPage'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <TodoPage />
   </StrictMode>,
